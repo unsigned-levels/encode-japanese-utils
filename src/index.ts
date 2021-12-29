af186e221b007bf59b8f00bdc16e359023475039
unsigned-levels <unsigned.levels@gmail.com>
2021-12-29 22:49:15 +0900

    セットアップ

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+const 関数名1 = (): void => {
+  console.log('ENCODE')
+  console.log('ENCODE')
+  console.log('ENCODE')
+  console.log('ENCODE')
+  console.log('ENCODE')
+  console.log('ENCODE')
+  console.log('ENCODE')
+  console.log('ENCODE')
+  console.log('ENCODE')
+}
+関数名1()
