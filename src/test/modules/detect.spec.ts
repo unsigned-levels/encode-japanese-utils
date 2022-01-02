eaa8985d23653392f97e287aeb4bb26249dd8394
unsigned-levels <unsigned.levels@gmail.com>
2022-01-03 03:07:22 +0900

    テスト名を変更

--- src/test/modules/detect.spec.ts
+++ src/test/modules/detect.spec.ts
@@ -3,12 +3,12 @@ import { findWords } from '@/modules/detect'
 import { rIt } from '@/test/utils/testUtil'
 
 rIt(盲目の読みがモウモクになるか)
-rIt(清水寺の読みがキヨミズデラになるか)
+rIt(清水寺の読みがセイスイジになるか)
 
 function 盲目の読みがモウモクになるか () {
   expect(findWords('盲目')).toBe('モウモク')
 }
 
-function 清水寺の読みがキヨミズデラになるか () {
+function 清水寺の読みがセイスイジになるか () {
   expect(findWords('清水寺')).toBe('セイスイジ')
 }
